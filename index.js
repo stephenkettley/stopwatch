a703a54d86b7f7d8a7986754d14467861fb3818f
Stephen Kettley <kettleystephen@gmail.com>
2023-09-06 15:02:15 +0200

    finalise app functionality

--- index.js
+++ index.js
@@ -3,11 +3,53 @@ const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const resetButton = document.getElementById("reset");
 
-function startTimer() {}
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
 
-function stopTimer() {}
+function startTimer() {
+  startTime = Date.now() - elapsedTime;
 
-function resetTimer() {}
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    timerElement.textContent = formatTime(elapsedTime);
+  }, 10);
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function formatTime(elapsedTime) {
+  const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+  const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+  const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+  const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+
+  return (
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    ":" +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
+    "." +
+    (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+  );
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  elapsedTime = 0;
+  timerElement.textContent = "00:00:00.00";
+}
 
 startButton.addEventListener("click", startTimer);
 stopButton.addEventListener("click", stopTimer);
