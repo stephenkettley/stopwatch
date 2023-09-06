c8bdee83eac99f9dfda818245a1443baa1871b20
Stephen Kettley <kettleystephen@gmail.com>
2023-09-06 09:56:47 +0200

    update js functionality

---
+++ index.js
@@ -0,0 +1,14 @@
+const timerElement = document.getElementById("timer");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+
+function startTimer() {}
+
+function stopTimer() {}
+
+function resetTimer() {}
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);
